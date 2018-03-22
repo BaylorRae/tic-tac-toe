78c46a8520af7342fb017eac4036c1fd52722357
Baylor Rae <baylor@thecodedeli.com>
2018-03-22 18:33:14 -0500

    moves can be made

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,38 @@
 require 'terminal-table'
 
 class Board
+  attr_reader :current_player, :squares
 
-  attr_reader :squares
+  PLAYER_1 = 'x'
+  PLAYER_2 = 'o'
 
   def initialize
-    @squares = (['-'] * 9).each_slice(3).map { |r| r }
+    @current_player = PLAYER_1
+    @squares = ['-'] * 9
   end
 
   def draw
-    puts Terminal::Table.new rows: squares
+    puts Terminal::Table.new rows: sliced_squares
+  end
+
+  def make_move(position)
+    return false unless valid_move?(position)
+    squares[position] = current_player
+    next_player
+  end
+
+  private
+
+  def sliced_squares
+    squares.each_slice(3).map { |r| r }
+  end
+
+  def next_player
+    @current_player = @current_player == PLAYER_1 ? PLAYER_2 : PLAYER_1
+  end
+
+  def valid_move?(position)
+    squares[position] == '-'
   end
 
 end
